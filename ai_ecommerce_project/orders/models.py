1e771899c38bc90e1e8d852591443f4abfba8fe6
Dermot <dermotag@hotmail.com>
2024-12-04 00:45:03 +0000

    Fixed the order API's fucntionality, will require documenting

--- ai_ecommerce_project/orders/models.py
+++ ai_ecommerce_project/orders/models.py
@@ -17,7 +17,7 @@ class Order(models.Model):
         max_length=20, choices=STATUS_CHOICES, default='pending')
     order_date = models.DateField(auto_now_add=True)
     shipping_date = models.DateField(null=True, blank=True)
-    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
 
     def __str__(self):
         return f'Order {self.order_number} - {self.user.username}'
