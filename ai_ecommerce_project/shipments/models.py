4ae496bf89df6fb3c785296bb215835aa272ab20
Dermot <dermotag@hotmail.com>
2025-01-18 18:53:53 +0000

    shipment models tweaked to reference whole order object

--- ai_ecommerce_project/shipments/models.py
+++ ai_ecommerce_project/shipments/models.py
@@ -5,11 +5,11 @@ from orders.models import Order
 class Shipments(models.Model):
     shipping_id = models.AutoField(primary_key=True)  
     product = models.ForeignKey(Product, on_delete=models.CASCADE) 
-    order_number = models.ForeignKey(Order, on_delete=models.CASCADE) 
+    order = models.ForeignKey(Order, on_delete=models.CASCADE) 
     card_transaction_time = models.DateTimeField()  # Timestamp of the card transaction
     packing_time = models.DateTimeField()  # Timestamp when the product was packed
     shipping_order_time = models.DateTimeField()  # Timestamp when the shipping order was placed
 
     def __str__(self):
-        return f"Shipment {self.shipping_id} for Order {self.order_number}"
+        return f"Shipment {self.shipping_id} for Order {self.order}"
 
