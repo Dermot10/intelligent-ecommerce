274577a01410931fdccc499813962045c93a7166
Dermot <dermotag@hotmail.com>
2025-02-03 23:45:51 +0000

    changes to payments view and serialiser

--- ai_ecommerce_project/payments/views.py
+++ ai_ecommerce_project/payments/views.py
@@ -1,3 +1,30 @@
+from urllib import request
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render
+from orders.models import Order, OrderItem
+from orders.serializers import OrderItemSerializer
+from .models import Payment
 
-# Create your views here.
+import logging 
+
+logger = logging.getLogger(__name__)
+
+class PaymentViewSet(viewsets.ModelViewSet): 
+    queryset = Payment.objects.all()
+    serializer_class = ... #PaymentSerializer
+
+    def get_order_for_user(self): 
+        pass 
+
+    def get_serializered_order_for_user(self): 
+        pass
+
+    def set_order_payment_pending(self): 
+        pass 
+
+    def update_order_payment_status(self): 
+        pass 
+
